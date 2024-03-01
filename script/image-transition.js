3cef6c4add4d4cfc0645dec6df84e7c56973f2ea
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-03-01 09:47:36 +0100

    Image Transition js added

---
+++ script/image-transition.js
@@ -0,0 +1,14 @@
+var ImgBox = document.querySelector(".img-box");
+var ImgWrap = document.querySelector(".img-wrap");
+var orginalImg = document.getElementById("orginalImg");
+var line = document.getElementById("line");
+
+orginalImg.style.width = ImgBox.offsetWidth + "px";
+
+var leftSpace = ImgBox.offsetLeft;
+
+ImgBox.onmousemove = function(e){
+  var boxWidth = (e.pageX - leftSpace) + "px";
+  ImgWrap.style.width = boxWidth;
+  line.style.left = boxWidth;
+}
